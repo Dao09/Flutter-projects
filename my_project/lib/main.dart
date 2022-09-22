40cd0bc30cf6c531b7555e7af7ad161fe867f16e
SRS <srsms43@gmail.com>
2022-09-22 13:47:44 +0530

    ch2 done

--- my_project/lib/main.dart
+++ my_project/lib/main.dart
@@ -16,6 +16,38 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("New App"),
         ),
-        body: Container(child: Text("Hello Flutter ")));
+        body: Container(
+          color: Colors.teal,
+          width: 300,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  //clipBehavior: Clip.antiAlias,
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  //clipBehavior: Clip.antiAlias,
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  //clipBehavior: Clip.antiAlias,
+                  width: 100,
+                  height: 100,
+                  color: Colors.lightGreen,
+                )
+              ]),
+        ));
   }
 }
