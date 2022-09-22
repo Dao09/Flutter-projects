2ca5b45e1a20a2fe84570bb00c14b83044712ac2
SRS <srsms43@gmail.com>
2022-09-22 15:49:31 +0530

    ch3 done

--- my_project/lib/main.dart
+++ my_project/lib/main.dart
@@ -13,41 +13,39 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("New App"),
+      appBar: AppBar(
+        title: Text("New App"),
+      ),
+      body:
+          Center(child: Container(height: 100, width: 100, color: Colors.teal)),
+      drawer: Drawer(
+          child: ListView(padding: EdgeInsets.all(0), children: <Widget>[
+        UserAccountsDrawerHeader(
+          accountName: Text("srs"),
+          accountEmail: Text("srs@gmail.com"),
+          currentAccountPicture: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://images.unsplash.com/photo-1663825162561-8399b6907932?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+          ),
         ),
-        body: Container(
-          color: Colors.teal,
-          width: 300,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(8),
-                  alignment: Alignment.center,
-                  //clipBehavior: Clip.antiAlias,
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  alignment: Alignment.center,
-                  //clipBehavior: Clip.antiAlias,
-                  width: 100,
-                  height: 100,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  alignment: Alignment.center,
-                  //clipBehavior: Clip.antiAlias,
-                  width: 100,
-                  height: 100,
-                  color: Colors.lightGreen,
-                )
-              ]),
-        ));
+        ListTile(
+          leading: Icon(Icons.person),
+          title: Text("xyz"),
+          subtitle: Text("pqr"),
+          trailing: Icon(Icons.edit),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.person),
+          title: Text("email"),
+          subtitle: Text("pqr@gnmail.com"),
+          trailing: Icon(Icons.email),
+        )
+      ])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
+    );
   }
 }
