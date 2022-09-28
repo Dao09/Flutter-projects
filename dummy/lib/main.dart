bd8c99f9bd59f8734483efcb0c07a41e8a19c7f6
SRS <srsms43@gmail.com>
2022-09-28 19:07:41 +0530

    n n  n

---
+++ dummy/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:dummy/home_page.dart';
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.amber
+      ),
+        darkTheme:ThemeData(
+          brightness: Brightness.dark
+        ) ,
+      routes: {
+        "/":(context) => LoginPage(),
+        "/home":(context) => HomePage()
+      }
+    );
+  }
+}
+
